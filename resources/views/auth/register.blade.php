d85948e914a923046cb8ff2f622e4cd804ee14b0
ronitm1 <121118391+ronitm1@users.noreply.github.com>
2023-01-09 00:23:33 +0100

    register front changed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,33 +2,6 @@
 
 @section('content')
 
-<!--
-=========================================================
-* Material Dashboard 2 - v3.0.4
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://www.creative-tim.com/license)
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
-
-   
-  
-
-<body class="">
-  <div class="container position-sticky z-index-sticky top-0">
-    <div class="row">
-      <div class="col-12">
-        <!-- Navbar -->
-        
-      </div>
-    </div>
-  </div>
   <main class="main-content  mt-0">
     <section>
       <div class="page-header min-vh-100">
@@ -40,39 +13,123 @@
             </div>
             <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5">
               <div class="card card-plain">
-                <div class="card-header">
+                <div class="card-header">  {{__('Register')}}
                   <h4 class="font-weight-bolder">Sign Up</h4>
-                  <p class="mb-0">Enter your email and password to register</p>
+                  <p class="mb-0">Enter your Personal Info-s to register</p>
                 </div>
+
                 <div class="card-body">
-                  <form role="form">
+                  <form role="form" method="POST" action="{{ route('register')}}">
+
+                    @csrf  
+                  <!--   NAME -->
                     <div class="input-group input-group-outline mb-3">
-                      <label class="form-label">Name</label>
-                      <input type="text" class="form-control">
+                      <label class="form-label">{{__('name')}}</label>
+                      <input for="name" id="name" name="name" type="text" class="form-control" value="{{old('name')}}" required autocomplete="name" autofocus>
                     </div>
+
+
+
+
+                 <!--   SURNAME-->
                     <div class="input-group input-group-outline mb-3">
-                      <label class="form-label">Email</label>
-                      <input type="email" class="form-control">
-                    </div>
+                        <label class="form-label">{{__('surname')}}</label>
+                        <input for="surname" id="surname" name="surname" type="text" class="form-control" value="{{old('surname')}}" required>
+
+                   @error('surname')
+                        <span class="invalid-feedback" role="alert">
+                           <strong>{{ $message }}</strong>
+                       </span>
+                    @enderror
+                </div>
+
+
+
+                       <!--   USERNAME   -->
+                      <div class="input-group input-group-outline mb-3">
+                        <label class="form-label">{{__('username')}}</label>
+                        <input for="username" id="username" name="username" type="text" class="form-control" value="{{old('username')}}" required>           
+                    
+                 @error('username')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                           </span>
+                      @enderror
+                   </div>
+
+
+            <!--  EMAIL  -->
                     <div class="input-group input-group-outline mb-3">
-                      <label class="form-label">Password</label>
-                      <input type="password" class="form-control">
+                      <label class="form-label">{{__('email')}}</label>
+                      <input for="email" id="email" name="email" type="email" class="form-control" {{old('email')}} required autocomplete="email">
+
+                   @error('email')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                            </span>
+                         @enderror
                     </div>
+
+
+             <!--   PASSWORD  -->
+                    <div class="input-group input-group-outline mb-3">
+                              <label class="form-label">{{__('password')}}</label>
+                 <input for="password" type="password" class="form-control" name="password" required autocomplete="new-password">
+
+
+                      @error('password')
+                          <span class="invalid-feedback" role="alert">
+                               <strong>{{ $message }}</strong>
+                             </span>
+                         @enderror
+                    </div>
+                      
+                    <!--   password_confirmation  -->
+
+                    <div class="input-group input-group-outline mb-3">
+                         <label class="form-label">{{__('password_confirmation')}}</label>
+                         <input for="password_confirmation" id="password_confirmation" name="confirm_password" type="password" class="form-control" required>
+                    </div>
+
+
+                    @error('confirm_password')
+                    <span class="invalid-feedback" role="alert">
+                         <strong>{{ $message }}</strong>
+                       </span>
+                   @enderror
+
+                 <!--  PHONE  -->
+             <div class="input-group input-group-outline mb-3">
+                    <label class="form-label">{{__('phone')}} </label>
+                    <input for="phone" id="phone" type="tel" class="form-control" @error('phone') is-invalid @enderror name="phone" value="{{ old('phone') }}" required autocomplete="phone" autofocus>
+                       
+                   @error('phone')
+                      <span class="invalid-feedback" role="alert">
+                         <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                 </div>
+
+
+
                     <div class="form-check form-check-info text-start ps-0">
                       <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
                       <label class="form-check-label" for="flexCheckDefault">
                         I agree the <a href="javascript:;" class="text-dark font-weight-bolder">Terms and Conditions</a>
                       </label>
                     </div>
+                    
                     <div class="text-center">
-                      <button type="button" class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">Sign Up</button>
+                      <button id="button" type="submit" class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">
+                        {{__('register')}}</button>
+                       
                     </div>
                   </form>
                 </div>
                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
                   <p class="mb-2 text-sm mx-auto">
                     Already have an account?
-                    <a href="../pages/sign-in.html" class="text-primary text-gradient font-weight-bold">Sign in</a>
+                    <a href="{{route('login')}}">Sign in</a>
                   </p>
                 </div>
               </div>
